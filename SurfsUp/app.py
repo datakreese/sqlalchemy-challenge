55ad4ffd9d8c3748199024a0cd42414c6ef560ba
Kathryn Reese <kathryn.ivyreese@gmail.com>
2024-05-01 10:42:52 -0600

    developer cleanup

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -20,8 +20,8 @@ Base = automap_base()
 Base.prepare(autoload_with=engine)
 
 # Save references to each table
-Station = Base.classes.station
-Measurement = Base.classes.measurement
+station = Base.classes.station
+measurement = Base.classes.measurement
 
 # Create our session (link) from Python to the DB
 session = Session(engine)
@@ -48,6 +48,7 @@ def welcome():
         f"/api/v1.0/start/end<br/>"
     )
 
+
 #####Get the precipitation data for the last year, store via dictionary
 @app.route("/api/v1.0/precipitation")
 def precipitation():
@@ -55,10 +56,9 @@ def precipitation():
     prev_year = dt.date(2017,8,23) - dt.timedelta(days=365)
     #session = Session(engine)
 
-    results = session.query(Measurement.prcp, Measurement.date).filter(Measurement.date >= prev_year).all()
+    results = session.query(measurement.prcp, measurement.date).filter(measurement.date >= prev_year).all()
     session.close()
 
-    #Do i even need a loop?
     precip = {date:prcp for prcp, date in results}
     return jsonify(precip)
 
@@ -67,43 +67,44 @@ def precipitation():
 #####list all stations as a Json object
 @app.route("/api/v1.0/stations")
 def Station_data():
-    #How could I list them in descending order?
-    # Query all passengers
-    results = session.query(Station.station, Station.name).all()
+    # Query all stations
+    results = session.query(station.station, station.name).all()
 
     session.close()
     stations = list(np.ravel(results))
     return jsonify(stations)
 
 
+
 #####return a JSON list for the temps in the last year from the most active station
 @app.route("/api/v1.0/tobs")
 def tobs():
-    temps = session.query(Measurement.tobs).filter(Measurement.date >= dt.date(2016, 8, 23)).\
-                        filter(Measurement.station == 'USC00519281').all()
+    temps = session.query(measurement.tobs).filter(measurement.date >= dt.date(2016, 8, 23)).\
+                        filter(measurement.station == 'USC00519281').all()
     session.close()
-    temperatures = {"Temperatures in the last year" : list(np.ravel(temps))}
+    temperatures = {"Temperatures in the last year for station USC00519281" : list(np.ravel(temps))}
     return jsonify(temperatures)
 
-## this route accepts user input
-## the user provides start date and end date
-#####Calculate min, mean, and max for temps within the specified dates
 
+
+## Designed in collaboration with tutor Hassan Mehmood 
+## this route accepts user input, the user provides start date and end date
+#####Calculate min, mean, and max for temps within the dynamic dates
 @app.route("/api/v1.0/<start>")
 @app.route("/api/v1.0/<start>/<end>")
 def startend(start=None, end=None):
-    partail_query = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+    partail_query = [func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)]
 
     if not end:
         start = dt.datetime.strptime(start,"%Y%m%d")
-        results = session.query(*partail_query).filter(Measurement.date >= start).all()
+        results = session.query(*partail_query).filter(measurement.date >= start).all()
         session.close()
         temps = list(np.ravel(results))
         return jsonify(temps)
     start = dt.datetime.strptime(start,"%Y%m%d")
     end = dt.datetime.strptime(end,"%Y%m%d")
 
-    results = session.query(*partail_query).filter(Measurement.date >= start).filter(Measurement.date<=end).all()
+    results = session.query(*partail_query).filter(measurement.date >= start).filter(measurement.date<=end).all()
     session.close()
     temps = list(np.ravel(results))
     return jsonify(temps)
